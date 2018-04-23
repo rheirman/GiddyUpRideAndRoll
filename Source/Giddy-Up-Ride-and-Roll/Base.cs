282401fc56a931f7c44e4a2ec9f7b40254f719f4
Roel <roelheirman@gmail.com>
2018-04-23 09:35:40 +0200

    Option to disable mounted hunting + no mounted hunting by default when using Combat Extended

--- Source/Giddy-Up-Ride-and-Roll/Base.cs
+++ Source/Giddy-Up-Ride-and-Roll/Base.cs
@@ -20,6 +20,7 @@ namespace GiddyUpRideAndRoll
         internal const string NOMOUNT_LABEL = "Gu_Area_NoMount";
         internal const string DROPANIMAL_LABEL = "Gu_Area_DropMount";
         internal static SettingHandle<int> minAutoMountDistance;
+        internal static SettingHandle<bool> noMountedHunting;
         internal static SettingHandle<int> minAutoMountDistanceFromAnimal;
 
         public override string ModIdentifier
@@ -32,7 +33,12 @@ namespace GiddyUpRideAndRoll
         }
         public override void DefsLoaded()
         {
+
+            bool defaultNoMountedHunting = AssemblyExists("CombatExtended");
+            Log.Message("Combat extended loaded: " + defaultNoMountedHunting);
             minAutoMountDistance = Settings.GetHandle<int>("minAutoMountDistance", "GU_RR_MinAutoMountDistance_Title".Translate(), "GU_RR_MinAutoMountDistance_Description".Translate(), 16, Validators.IntRangeValidator(0, 500));
+            noMountedHunting = Settings.GetHandle<bool>("noMountedHunting", "GU_RR_NoMountedHunting_Title".Translate(), "GU_RR_NoMountedHunting_Description".Translate(), defaultNoMountedHunting);
+
             minAutoMountDistanceFromAnimal = Settings.GetHandle<int>("minAutoMountDistanceFromAnimal", "GU_RR_MinAutoMountDistanceFromAnimal_Title".Translate(), "GU_RR_MinAutoMountDistanceFromAnimal_Description".Translate(), 12, Validators.IntRangeValidator(0, 500));
 
             PawnTableDef animalsTable = PawnTableDefOf.Animals;
@@ -50,6 +56,16 @@ namespace GiddyUpRideAndRoll
         public ExtendedDataStorage GetExtendedDataStorage()
         {
             return GiddyUpCore.Base.Instance.GetExtendedDataStorage();
+        }
+
+        private bool AssemblyExists(string assemblyName)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (assembly.FullName.StartsWith(assemblyName))
+                    return true;
+            }
+            return false;
         }
 
     }
