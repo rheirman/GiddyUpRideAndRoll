e5506e16ced6748ade43c72794a0e47cea13d38a
Roel <roelheirman@gmail.com>
2019-06-27 11:07:32 +0200

    no training mounted animals

---
+++ Source/Giddy-Up-Ride-and-Roll/Harmony/WorkGiver_Train.cs
@@ -0,0 +1,27 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+using GiddyUpCore.Utilities;
+
+namespace GiddyUpRideAndRoll.Harmony
+{
+    //Don't let pawns train mounted animals. 
+    [HarmonyPatch(typeof(WorkGiver_Train),"JobOnThing")]
+    class WorkGiver_Train_JobOnThing
+    {
+        static bool Prefix(WorkGiver_Train __instance, Pawn pawn, Thing t, ref Job __result)
+        {
+            if(t is Pawn animal && animal.RaceProps.Animal && IsMountableUtility.IsCurrentlyMounted(animal))
+            {
+                __result = null;
+                return false;
+            }
+            return true;
+        }
+    }
+}
