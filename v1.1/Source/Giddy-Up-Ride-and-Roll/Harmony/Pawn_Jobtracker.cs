4ba9d6dc322c587511f1634647777f3f0092d965
Gusgus01 <gus.gus.rocks@gmail.com>
2020-05-02 02:09:46 -0700

    Don't allow mounting if the current determined job is to Goto.
    Fixes rheirman/GiddyUpRideAndRoll#16
    
    This is a rough fix. It does not allow mounting to ride to a location
    (unless already mounted).

--- v1.1/Source/Giddy-Up-Ride-and-Roll/Harmony/Pawn_Jobtracker.cs
+++ v1.1/Source/Giddy-Up-Ride-and-Roll/Harmony/Pawn_Jobtracker.cs
@@ -42,6 +42,10 @@ namespace GiddyUpRideAndRoll.Harmony
             {
                 return;
             }
+            if (__result.Job.def.defName == "Goto")
+            {
+                return;
+            }
             ExtendedDataStorage store = Base.Instance.GetExtendedDataStorage();
             if(store == null)
             {
